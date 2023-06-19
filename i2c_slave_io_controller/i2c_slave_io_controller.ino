4e1acd15a8ace268a7ebc0d393b0d2f5e07d8f30
Tonokip <tonokip@gmail.com>
2023-06-19 12:52:14 -0500

    changes for bigger board with more ports

--- i2c_slave_io_controller/i2c_slave_io_controller.ino
+++ i2c_slave_io_controller/i2c_slave_io_controller.ino
@@ -4,7 +4,7 @@
 #define DEBUG_PRINTLN(x)
 
 void setup() {
-  Wire.begin(13);                // join i2c bus with address #8
+  Wire.begin(17);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // register event
   Wire.onRequest(requestEvent); // register event
   Serial.end();
@@ -16,6 +16,7 @@ void setup() {
 uint8_t pin;
 uint8_t values[70];
 uint8_t dataSize;
+uint8_t mode;
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -59,6 +60,32 @@ void receiveEvent(int howMany) {
       digitalWrite(buf[i], LOW);
     }
   }
+
+  if( buf[0] == 'E' ) {
+    for(uint8_t i = 1; i < dataSize; i++) {
+      values[i] = buf[i];
+    }
+  }
+
+  if( buf[0] == 'G' ) { // Check for bridges from first pin to any other other
+    for(int i = 0; i < sizeof(values); i++) values[i]=0;
+    pinMode(buf[1], OUTPUT);
+    digitalWrite(buf[1], LOW);
+    for(int j=2; j < dataSize ; j++)
+    {
+      if(digitalRead(buf[j]) == LOW)
+      {
+        values[j-2]=true;
+        /*
+        Serial.print("Driving pin ");
+        Serial.print(buf[i]);
+        Serial.print(" low causes pin ");
+        Serial.print(buf[j]);
+        Serial.println(" to be low."); */
+      }
+    } // For j
+    pinMode(buf[1], INPUT_PULLUP);
+  } // 'G'
 
   if( buf[0] == 'B' ) {
     for(int i = 0; i < sizeof(values); i++) values[i]=0;
--- rambo_i2c_boundary_scan6.ino
+++ rambo_i2c_boundary_scan6.ino
@@ -1,11 +1,28 @@
 #include <Wire.h>
 
+
+//#define TESTING
+
 uint8_t pins_J3[] = { 44, 46, 48, 50, 52, 69, 67, 65, 63, 61, 59, 57, 55, 45, 47, 49, 51, 53, 68, 66, 64, 62, 60, 58, 56 };
+
+uint8_t pins_J3_no6[] = { 44, 46, 48, 50, 52,     67, 65, 63, 61, 59, 57, 55, 45, 47, 49, 51, 53, 68, 66, 64, 62, 60, 58, 56 };
+uint8_t pins_J3_no8[] = { 44, 46, 48, 50, 52, 69, 67,     63, 61, 59, 57, 55, 45, 47, 49, 51, 53, 68, 66, 64, 62, 60, 58, 56 };
+
+
+#ifdef TESTING
+uint8_t pins_J4[] = { 44, 19, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42 };
+#else
 uint8_t pins_J4[] = { 17, 19, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 18, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42 };
-//uint8_t pins_J4[] = { 44, 19, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42 };
+#endif
+
 uint8_t    dbmp[] = {  1,  2,  3,  4,  5,  6,  7 , 8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+
+#ifdef TESTING
+uint8_t rj_out_J1[] = { 4,   3,  2, 17, 18, 14, 15, 16 };
+#else
 uint8_t rj_out_J1[] = { 4, 3, 2,  1,  0, 14, 15, 16 };
-//uint8_t rj_out_J1[] = { 4,   3,  2, 17, 18, 14, 15, 16 };
+#endif
+
 uint8_t rj_out_J2[] = { 12, 11, 10,  9,  8,  7,  6,  5 };
 uint8_t  db_in_J3[] = { 52, 65, 48, 67, 67, 46, 66, 50 };
 uint8_t  db_in_J4[] = { 27, 33, 23, 31, 31, 19, 32, 25 };
@@ -31,6 +48,10 @@ unsigned long startMillis;
 char currentChar; //current char we are processing
 int8_t pin; //current pin we are playing with
 
+uint8_t pins[70];
+uint8_t values[70];
+uint8_t index;
+
 void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
   Wire.setClock(10000);
@@ -38,6 +59,10 @@ void setup() {
   Serial.println("1");
   i2c_scan();
   pinMode(LED_PIN,OUTPUT);
+  Serial.println("start");
+
+  index = 0;
+  for(int i = 0; i < sizeof(index); i++) values[i]=0; //Clear values
 }
 
 #define DEVICE_1_ADDR 8
@@ -48,6 +73,7 @@ void finished(void){
 }
 
 void loop() {
+  /*
   if(!digitalRead(START_PIN))
   {
     startReads++;
@@ -56,7 +82,8 @@ void loop() {
       startReads = 0;
       startMillis = millis();
     }
-  }
+  }*/
+  static uint8_t address = 0;
 
   manage_blinker();
   
@@ -68,6 +95,8 @@ void loop() {
     delay(10);
     switch (currentChar)
     {
+   case '\n': break;
+   case '\r': break;
    case '?' :
      {
       Serial.println("Commands: ");
@@ -76,10 +105,84 @@ void loop() {
       Serial.println("J<addr> (J1J3 test for one i2c address)");
       Serial.println("K<addr> (J2J4 test for one i2c address)");
       Serial.println("F (Run full test)\n");
+      break;
      }
+    case 'U' : //Uptime
+      {
+        Serial.println(millis());
+        break;
+      }
       //Read Pin but turn pullups on
       //Format: Q<pin>
       //Returns: <pin val>\n
+    case 'a' :
+      {
+          if(isDigit(Serial.peek())){
+            address = Serial.parseInt();
+          }
+          else
+          {
+            Serial.print("address: ");
+            Serial.println(address);
+          }
+          break;
+      }
+    case 'i' :
+      {
+          if(isDigit(Serial.peek())){
+            index = Serial.parseInt();
+          }
+          else
+          {
+            Serial.print("index: ");
+            Serial.println(index);
+          }
+          break;
+      }
+    case 'w' :
+      {
+          if(isDigit(Serial.peek())){
+            values[index] = Serial.parseInt();
+            index++;
+          }
+          else
+          {
+            Serial.print("values: ");
+            for(int i = 0; i < sizeof(values); i++)
+            {
+              Serial.print(values[i]);
+              Serial.print(',');
+            }
+            Serial.println();
+          }
+          break;
+      }
+    case 'r' :
+      {
+          if( isDigit(Serial.peek()) )
+          {
+            uint8_t readCount = Serial.parseInt();
+            Wire.requestFrom(address,readCount);
+            uint32_t timeout = 10000; while( Wire.available() == false && timeout) { timeout--; } ;
+            while( Wire.available() ) {
+              uint8_t rxData = Wire.read();
+              Serial.print ( rxData );
+              Serial.print(',');
+            }
+            Serial.println();
+          }
+          break;
+      }
+    case 'b' :
+      {
+          J1J3_test(address);
+          break;
+      }
+    case 'g' :
+      {
+          J1J3_bridge_test(address);
+          break;
+      }
     case 'Q' : 
       {
         if(isDigit(Serial.peek())){
@@ -108,6 +211,16 @@ void loop() {
     case 'K' : 
       {
         if(isDigit(Serial.peek())){
+          uint8_t addr = Serial.parseInt();
+          Serial.println(J2J4_test(addr));
+          pin = -1;
+        }
+        finished();
+        break; 
+      }
+    case 'G' : 
+      {
+        if(Serial.peek()){
           uint8_t addr = Serial.parseInt();
           Serial.println(J2J4_test(addr));
           pin = -1;
@@ -150,7 +263,7 @@ void i2c_scan()
   Serial.println("Scanning...");
 
   nDevices = 0;
-  for (address = 1; address <= 16; address++ )
+  for (address = 1; address <= 20; address++ )
   {
     // The i2c_scanner uses the return value of
     // the Write.endTransmisstion to see if
@@ -230,6 +343,20 @@ uint8_t read_i2c_pin(uint8_t addr, uint8_t pin)
   return -1;
 }
 
+int8_t J1J3_bridge_test(uint8_t addr)
+{
+  int8_t retval;
+  uint32_t start_time = millis();
+  DEBUG_PRINT("\nJ1J3 test address: ");
+  DEBUG_PRINT(addr);
+  DEBUG_PRINT(" cable: ");
+  DEBUG_PRINT((addr - 1) * 2);
+  retval = bridge_test(addr, pins_J3, sizeof(pins_J3) );
+  //DEBUG_PRINT("Test1 Time: ");
+  //DEBUG_PRINTLN(millis() - start_time);
+  return retval;
+}
+
 int8_t J1J3_test(uint8_t addr)
 {
   int8_t retval;
@@ -256,6 +383,37 @@ int8_t J2J4_test(uint8_t addr)
   //DEBUG_PRINT("Test1 Time: ");
   //DEBUG_PRINTLN(millis() - start_time);
   return retval;
+}
+
+int8_t bridge_test(uint8_t addr, uint8_t pins[], uint8_t pinCount)
+{
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  uint8_t test_pass=0;
+
+  // Bridged pins Test
+  Wire.beginTransmission(addr);
+  Wire.write('G');
+  Wire.write(pins, pinCount);
+  Wire.endTransmission();
+
+  Wire.requestFrom(addr, pinCount);
+  Serial.print(" B:");
+  for(int i = 0; i < pinCount; i++)
+  {
+    if( ! Wire.available() ) {
+      Serial.print(" ?");
+      test_pass++;
+      continue;
+    } else {
+      uint8_t c = Wire.read(); // receive a byte as character
+      Serial.print(',');
+      Serial.print(c);
+      if( c ) test_pass++;
+    }
+  }
+  Serial.println();
+  return test_pass;
 }
 
 int8_t test(uint8_t addr, uint8_t pins[], uint8_t pinCount, uint8_t rj_out[], uint8_t db_in[], uint8_t rjCount) {
@@ -293,6 +451,8 @@ int8_t test(uint8_t addr, uint8_t pins[], uint8_t pinCount, uint8_t rj_out[], ui
   Wire.write('B');
   Wire.write(pins, pinCount);
   Wire.endTransmission();
+
+  
 
   Wire.requestFrom(addr, pinCount);
   Serial.print(" B:");
